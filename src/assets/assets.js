922725e548695745b7591e0f124cd33b85a6a7cb
asvaruda <gdr20021019@gmail.com>
2024-12-30 23:13:01 +0530

    records details updated

---
+++ src/assets/assets.js
@@ -0,0 +1,21 @@
+import rec_1 from "./rec_img_1.jpeg";
+import rec_2 from "./rec_img_2.jpeg";
+
+export const record_list = [
+  {
+    _id: "1",
+    name: "M. Harshika Sets World Record with 30 Yoga Asanas in 2 Minutes",
+    image: rec_1,
+    date: "02-12-2024",
+    description:
+      "M. Harshika 10-year-old from Madurai has set a remarkable world record by performing 30 yoga asanas continuously in just 2 minutes. Her achievement, which included challenging poses like the full-wheel posture and advanced balancing asanas, has earned her a prestigious award and recognition. A student of Annai Yoga Center, Harshika’s dedication and skill have inspired many young yoga enthusiasts. This extraordinary accomplishment highlights the potential of young talents in mastering ancient practices like yoga. Harshika’s success is a proud moment for her family, trainers, and the entire community. Congratulations to this young achiever!",
+  },
+  {
+    _id: "2",
+    name: "KRIET record event on mental health day",
+    image: rec_2,
+    date: "11-10-2023",
+    description:
+      "To mark World Mental Health Day, KPR Institute of Engineering and Technology in Coimbatore achieved two world records to highlight the importance of mental health. The institute hosted a bike rally with the most participants and a laughter yoga session with the largest gathering, both taking place at the KPRIET campus.",
+  },
+];
---
+++ src/components/records/Search.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { record_list } from "../../assets/assets.js";
+import { Switch, Typography } from "antd";
+import "./Search.css";
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredrecords, setFilteredrecords] = useState(record_list);
+  const [ellipsis, setEllipsis] = useState(true);
+  const { Paragraph } = Typography;
+  function handleSearch(e) {
+    const searchValue = e.target.value;
+    setSearchTerm(searchValue);
+    const filtered = record_list.filter((record) =>
+      record.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredrecords(filtered);
+  }
+  return (
+    <>
+      <div className="rec-nav"></div>
+      <div className="hero-buttons search-btn">
+        <input
+          type="text"
+          placeholder="Search for a record..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <button type="button" className="btn btn-red">
+          Search
+        </button>
+      </div>
+      {filteredrecords.length === 0 ? (
+        <p className="not-found">record not found</p>
+      ) : (
+        <div className="record-grid">
+          {filteredrecords.map((record) => (
+            <div key={record.id} className="records-margine">
+              <div className="records-card">
+                <img
+                  src={record.image}
+                  alt={record.name}
+                  className="record-img"
+                />
+                <div className="record-content">
+                  <h2 className="common-colour record-title">{record.name}</h2>
+                  <h3 className="common-colour record-date">
+                    Held at : {record.date}
+                  </h3>
+
+                  <Switch
+                    checked={ellipsis}
+                    onChange={() => {
+                      setEllipsis(!ellipsis);
+                    }}
+                    style={{ display: "none" }}
+                  />
+
+                  <Paragraph
+                    className="antd-style common-colour record-overview"
+                    ellipsis={
+                      ellipsis
+                        ? { rows: 6, expandable: true, symbol: "more" }
+                        : false
+                    }
+                  >
+                    {record.description}
+                  </Paragraph>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Search;
